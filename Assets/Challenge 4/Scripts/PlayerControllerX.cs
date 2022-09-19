cff21efb3e1e71bb1e7f8e5889ff72512716a9da
Emegbue Chukwudi John <ecj4real@gmail.com>
2022-09-19 23:34:01 +0100

    Fix issue "The player needs a turbo boost"

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -14,6 +14,8 @@ public class PlayerControllerX : MonoBehaviour
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
+
+    public ParticleSystem smokeParticle;
     
     void Start()
     {
@@ -29,6 +31,12 @@ public class PlayerControllerX : MonoBehaviour
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(focalPoint.transform.forward * 10, ForceMode.Impulse);
+            smokeParticle.Play();
+        }
 
     }
 
